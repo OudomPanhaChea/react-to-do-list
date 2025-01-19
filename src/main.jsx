6c435e1cb9e278e35998322a56aa9286c4d87ebd
OudomPanhaChea <169124803+OudomPanhaChea@users.noreply.github.com>
2025-01-19 22:51:19 +0700

    add base

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
-import { HasRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HasRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  </HasRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
